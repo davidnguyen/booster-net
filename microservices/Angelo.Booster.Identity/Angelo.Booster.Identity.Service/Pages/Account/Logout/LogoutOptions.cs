c36082e8a4c66e4755ea0fe82ded97d0a8482594
David Nguyen <diepnn@gmail.com>
2022-04-01 16:39:50 -0700

    Remove old identity service

--- microservices/Angelo.Booster.Identity/Angelo.Booster.Identity.Service/Pages/Account/Logout/LogoutOptions.cs
+++
@@ -1,8 +0,0 @@
-
-namespace Angelo.Booster.Identity.Service.Pages.Logout;
-
-public class LogoutOptions
-{
-    public static bool ShowLogoutPrompt = true;
-    public static bool AutomaticRedirectAfterSignOut = false;
-}
