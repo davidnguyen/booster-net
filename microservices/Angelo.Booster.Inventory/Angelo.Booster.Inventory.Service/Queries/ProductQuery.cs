44cf6626369b6123671a8ae25c98e1bc470d4504
David Nguyen <diepnn@gmail.com>
2022-04-19 00:15:00 -0700

    Add some xml documentation for product query

--- microservices/Angelo.Booster.Inventory/Angelo.Booster.Inventory.Service/Queries/ProductQuery.cs
+++ microservices/Angelo.Booster.Inventory/Angelo.Booster.Inventory.Service/Queries/ProductQuery.cs
@@ -4,11 +4,23 @@ namespace Angelo.Booster.Inventory.Service.Queries;
 
 public class ProductQuery
 {
-    public IEnumerable<Product> GetProducts(ApplicationDbContext dbContext)
+    /// <summary>
+    /// Gets products by category
+    /// </summary>
+    /// <param name="dbContext">Database context</param>
+    /// <param name="category">Category to fetch products for</param>
+    /// <returns>Products</returns>
+    public IEnumerable<Product> GetProducts(ApplicationDbContext dbContext, string category)
     {
         return dbContext.Products.ToArray();
     }
 
+    /// <summary>
+    /// Gets product details by productId
+    /// </summary>
+    /// <param name="dbContext">Database context</param>
+    /// <param name="productId">Product identifier</param>
+    /// <returns>Product</returns>
     public Product GetProduct(ApplicationDbContext dbContext, string productId)
     {
         var product = dbContext.Products.SingleOrDefault(x => x.Id == new Guid(productId));
